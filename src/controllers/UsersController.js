9f671f762bd249b44f1228fe8798c502422e709a
Bruno Duarte <brunosduarte@hotmail.com>
2024-01-12 22:07:24 -0300

    making routes

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -24,30 +24,30 @@ class UsersController {
   }
 
   async update(request, response) {
-     const { name, email, password, old_password } = request.body
-     const { id } = request.params
+    const { name, email, password, old_password } = request.body
+    const { id } = request.params
 
-     const database = await sqliteConnection()
-     const user = await database.get("SELECT * FROM users WHERE id = (?)", [id])
+    const database = await sqliteConnection()
+    const user = await database.get("SELECT * FROM users WHERE id = (?)", [id])
 
-     if(!user) {
+    if(!user) {
       throw new AppError("Usuário não encontrado")
-     }
+    }
 
-     const userWithUpdatedEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email])
+    const userWithUpdatedEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email])
 
-     if(userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
+    if(userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
       throw new AppError("Este e-mail já está em uso.")
-     }
+    }
 
-     user.name = name ?? user.name
-     user.email= email ?? user.email
+    user.name = name ?? user.name
+    user.email= email ?? user.email
 
-     if(password && !old_password) {
+    if(password && !old_password) {
       throw new AppError("Você informar a senha antiga para definir a nova senha")
-     }
+    }
 
-     if(password && old_password) {
+    if(password && old_password) {
       const checkOldPassword = await compare(old_password, user.password)
 
       if(!checkOldPassword) {
@@ -55,9 +55,9 @@ class UsersController {
       }
 
       user.password = await hash(password, 8)
-     }
+    }
 
-     await database.run(`
+    await database.run(`
       UPDATE users SET
       name = ?,
       email = ?,
